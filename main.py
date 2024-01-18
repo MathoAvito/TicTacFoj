4785979404f0f615132f66f57d740e2bcef197f5
omer <bhomer1995@gmail.com>
2024-01-18 14:40:06 +0200

    changed logic

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 # Initialize the game matrix
-matrix = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+matrix = [[0,0,0], [0,0,0], [0,0,0]]
 
 def check_status(matrix):
     for row in range(3):  # check rows
         if sum(matrix[row]) == 3:
             return "the guy with the X's won"
-        elif sum(matrix[row]) == 0:
+        elif sum(matrix[row]) == -3:
             return "the guy with the O's won"
 
     for col in range(3):  # check cols
         if matrix[0][col] + matrix[1][col] + matrix[2][col] == 3:
             return "the guy with the X's won"
-        elif matrix[0][col] + matrix[1][col] + matrix[2][col] == 0:
+        elif matrix[0][col] + matrix[1][col] + matrix[2][col] == -3:
             return "the guy with the O's won"
 
     # diagonals
@@ -25,15 +25,17 @@ def check_status(matrix):
     secondary_diag = matrix[0][2] + matrix[1][1] + matrix[2][0]
     if main_diag == 3 or secondary_diag == 3:
         return "the guy with the X's won"
-    if main_diag == 0 or secondary_diag == 0:
+    if main_diag == -3 or secondary_diag == -3:
         return "the guy with the O's won"
 
     return "continue playing"
 
 def is_legal_move(i, j):
-    if matrix[i][j] != -1:
+    if matrix[i][j] != 0:
         return False
     return True
+
+
 
 @app.route('/')
 def index():
